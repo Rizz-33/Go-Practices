0a9bbfa4586af8b3104be60136b88537d2c39f50
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-26 18:24:03 +0530

    Print Strings

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the Conference Booking Application")
 	fmt.Println("Get your tickets here to attend")
+
+	var conName = "Go Conference"
+	fmt.Print(conName)
 }
