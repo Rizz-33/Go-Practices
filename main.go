897e0e42350d2997830868c1d2fb11affc5e01a8
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-26 19:45:47 +0530

    User Input

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var conName = "Go Conference"
-	const conTickets = 50
-	var remainingTickets = 50
+	conName := "Go Conference"
+	const conTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conTickets is %T remainingTickets is %T, conName is %T\n", conTickets, remainingTickets, conName)
 
 	fmt.Printf("Welcome to the %v Booking Application!\n", conName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend...")
+
+	var userName string
+	var userTickets int
+	fmt.Scan(&userName)
+
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
