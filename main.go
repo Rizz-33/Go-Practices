5680d98617def75bbabf6c005de3cbb7338a592c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-26 18:20:27 +0530

    refactor: Println

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println("Welcome to the Conference Booking Application")
+	fmt.Println("Get your tickets here to attend")
 }
